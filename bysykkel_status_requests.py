975c04b107ea3360878bc0906c082f0e5ddfe116
Knut <knut.helland@gmail.com>
2019-11-24 14:23:54 +0100

    Omstrukturerte koden for enklere testing og videreutvikling

---
+++ bysykkel_status_requests.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+
+
+def bysykkel_status_request(client_name = "personlig-bysykkel_status"):
+    return requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json"
+                                       , headers={"client-name": client_name})
+def bysykkel_info_request(client_name = "personlig-bysykkel_status"):
+    return  requests.get("https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
+                                     , headers={"client-name": client_name})
+
+def get_table():
+
+    table = []
+    data_received = False
+
+
+    try:
+        response_status = bysykkel_status_request()
+        response_info = bysykkel_info_request()
+    except requests.exceptions.ConnectionError:
+        print("Could not connect to bysykkel-API")
+        return table, data_received
+
+    if response_status.status_code==200 and response_info.status_code==200:
+        try:
+            for station_info in response_info.json()['data']['stations']:
+                for station_status in response_status.json()['data']['stations']:
+                    if station_info['station_id'] == station_status['station_id']:
+                        table.append((station_info['name'],
+                                      station_status['num_bikes_available'],
+                                      station_status['num_docks_available']
+                        ))
+                        break
+            table.sort(key=lambda x: x[0])
+            data_received = True
+        except (KeyError, IndexError, TypeError):
+            print("Received data not in expected form")
+    else:
+        print("Unexpected status codes:")
+        print("Expected 200 for station status request, received " +str(response_status.status_code))
+        print("Expected 200 for station information request, received " + str(response_info.status_code))
+
+    return table, data_received
